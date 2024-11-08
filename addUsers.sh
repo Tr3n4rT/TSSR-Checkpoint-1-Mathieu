0940b6581b95d5876152ea8a94565ab8bd0f4d61
Tr3n4rT M4tth <tr3n4rt@pm.me>
2024-11-08 14:47:34 +0100

    ajout du script

--- addUsers.sh
+++ addUsers.sh
@@ -1,1 +1,29 @@
-#Exercice 2 - SCRIPT CREATION D'UTILISATEUR EN BASH
+#/bin/bash
+#Checkpoint 1 - Exercice 2 - SCRIPT CREATION D'UTILISATEUR EN BASH
+
+# Vérification d'au moins un argument
+if [ "$#" -lt 1 ]
+then
+	echo "Il manque les noms d'utilisateurs en argument.
+	Fin du script "
+	exit 1
+fi
+
+# Boucle for pour créer chaque utilisateur passés en argument
+for nouvel_utilisateur in "$@" 
+do
+	# Vérifie si le nouvel utilisateur existe déjà
+	if id $nouvel_utilisateur >/dev/null 2>&1
+	then
+	echo "L'utilisateur $nouvel_utilisateur existe déjà. "
+	else
+	# Création du nouvel utilisateur
+	sudo useradd $nouvel_utilisateur
+	if [ $? -eq 0 ]
+	then
+		echo "L'utilisateur $nouvel_utilisateur a bien été crée. "
+	else
+		echo "Erreur lors de la création de l'utilisateur $nouvel_utilisateur."
+	fi
+fi
+done
